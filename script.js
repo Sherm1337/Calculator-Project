23ca0f6a16bc2e90e4c88ec7d1588ac94a01d0b2
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-03-31 22:02:14 -0500

    Rework functions
    -Restructured all of the button functions
    -Modified CSS to make it easier to read
    -Fixed know bugs as shown on TOP lesson page
    -Added Phone support
    
    To Be Added:
    -Keyboard compatibility
    -Backspace button
    -More functions

--- script.js
+++ script.js
@@ -2,33 +2,15 @@ var visual = document.querySelector(".visuals");
 
 visual.textContent = 0;
 
-var operator = "";
+var operator = null;
 var total = 0;
 
+var runningTotal = false;
 var hasDecimal = false;
 var isPercent = false;
 
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero");
-
-const equals = document.querySelector("#equals");
-const addition = document.querySelector("#addition");
-const subtraction = document.querySelector("#subtraction");
-const multiplication = document.querySelector("#multiplication");
-const division = document.querySelector("#division");
-const changeSign = document.querySelector("#changeSign");
-const decimal = document.querySelector("#decimalPoint");
-const percent = document.querySelector("#percent");
-
-const buttons = document.querySelectorAll(".button");
+const equals = document.querySelector(".equals");
+const buttons = document.querySelectorAll("button");
 
 var array = [];
 
@@ -38,205 +20,122 @@ if(visual.textContent.includes(".")){
     hasDecimal = false;
 }
 
-one.onclick = () => {
+function buttonClicked () {
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", () => {
+            if(buttons[i].classList.contains("number")) {
+                if(runningTotal == true){
+                    visual.textContent = 0;
+                    inputNumber(buttons[i].value);
+                    runningTotal = false;
+                }else if(visual.textContent.length < 23){
+                    inputNumber(buttons[i].value);
+                }
+            }else if(buttons[i].classList.contains("zero")){
+                if(visual.textContent !== '0'){
+                    visual.textContent += 0;
+                }
+            }else if(buttons[i].classList.contains("changeSign")) {
+                if(parseFloat(visual.textContent) > 0) {
+                    visual.textContent = "-" + parseFloat(visual.textContent);
+                    visual.textContent = parseFloat(visual.textContent);
+                }else if (parseFloat(visual.textContent) < 0) {
+                    visual.textContent = parseFloat(visual.textContent) * -1;
+                }
+            }else if(buttons[i].classList.contains("operator")) {
+                if(runningTotal == true){
+                    visual.textContent = 0;
+                    operator = buttons[i].value;
+                }else{
+                    operator = buttons[i].value;
+                    preOperate();
+                }
+                
+            }else if(buttons[i].classList.contains("decimalPoint")) {
+                if(hasDecimal == false){
+                    visual.textContent += ".";
+                }
+            }else if(buttons[i].classList.contains("percent")) {
+                if(isPercent == true) {
+                    visual.textContent = visual.textContent * 100;
+                    isPercent = false;
+                } else if(isPercent == false) {
+                    visual.textContent = visual.textContent / 100;
+                    isPercent = true;
+                }
+            }
+    })
+    buttons[i].addEventListener("ontouchstart", () => {
+        if(buttons[i].classList.contains("number")) {
+            if(runningTotal == true){
+                visual.textContent = 0;
+                inputNumber(buttons[i].value);
+                runningTotal = false;
+            }else if(visual.textContent.length < 23){
+                inputNumber(buttons[i].value);
+            }
+        }else if(buttons[i].classList.contains("zero")){
+            if(visual.textContent !== '0'){
+                visual.textContent += 0;
+            }
+        }else if(buttons[i].classList.contains("changeSign")) {
+            if(parseFloat(visual.textContent) > 0) {
+                visual.textContent = "-" + parseFloat(visual.textContent);
+                visual.textContent = parseFloat(visual.textContent);
+            }else if (parseFloat(visual.textContent) < 0) {
+                visual.textContent = parseFloat(visual.textContent) * -1;
+            }
+        }else if(buttons[i].classList.contains("operator")) {
+            if(runningTotal == true){
+                visual.textContent = 0;
+                operator = buttons[i].value;
+            }else{
+                operator = buttons[i].value;
+                preOperate();
+            }
+            
+        }else if(buttons[i].classList.contains("decimalPoint")) {
+            if(hasDecimal == false){
+                visual.textContent += ".";
+            }
+        }else if(buttons[i].classList.contains("percent")) {
+            if(isPercent == true) {
+                visual.textContent = visual.textContent * 100;
+                isPercent = false;
+            } else if(isPercent == false) {
+                visual.textContent = visual.textContent / 100;
+                isPercent = true;
+            }
+        }
+})
+    }
+}
+buttonClicked();
+
+function inputNumber(input) {
     if(visual.textContent === '0') {
-        visual.textContent = one.textContent;
+        visual.textContent = input;
     }else {
-        visual.textContent += one.textContent;
+        visual.textContent += input;
     }
 }
 
-two.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = two.textContent;
-    }else {
-        visual.textContent += two.textContent;
-    }
-}
-
-three.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = three.textContent;
-    }else {
-        visual.textContent += three.textContent;
-    }
-}
-
-four.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = four.textContent;
-    }else {
-        visual.textContent += four.textContent;
-    }
-}
-
-five.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = five.textContent;
-    }else {
-        visual.textContent += five.textContent;
-    }
-}
-
-six.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = six.textContent;
-    }else {
-        visual.textContent += six.textContent;
-    }
-}
-
-seven.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = seven.textContent;
-    }else {
-        visual.textContent += seven.textContent;
-    }
-}
-
-eight.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = eight.textContent;
-    }else {
-        visual.textContent += eight.textContent;
-    }
-}
-
-nine.onclick = () => {
-    if(visual.textContent === '0') {
-        visual.textContent = nine.textContent;
-    }else {
-        visual.textContent += nine.textContent;
-    }
-}
-
-zero.onclick = () => {
-    if(visual.textContent === '0'){
-        return;
-    }else {
-        visual.textContent += zero.textContent;
-    }
-}
-
-changeSign.onclick = () => {
-    if (parseFloat(visual.textContent) > 0) {
-        visual.textContent = "-" + parseFloat(visual.textContent);
-        visual.textContent = parseFloat(visual.textContent);
-    }else if (parseFloat(visual.textContent) < 0) {
-        visual.textContent = parseFloat(visual.textContent) * -1;
-    }
-}
-
-decimal.onclick = () => {
-    if(hasDecimal === false) {
-    visual.textContent = parseFloat(visual.textContent) + ".";
-    hasDecimal = true;
-    }
-}
-
-percent.onclick = () => {
-    if(isPercent == true) {
-        visual.textContent = parseFloat(visual.textContent) * 100;
-        isPercent = false;
-    }else if(isPercent == false) {
-        visual.textContent = parseFloat(visual.textContent) / 100;
-        isPercent = true;
-    }
-
-}
-
-addition.onclick = () => {
-    hasDecimal = false;
-    if(total !== 0) {
-        array.splice(0,0, parseFloat(total));
-        operator = "+"
-        total = 0;
-        visual.textContent = 0;
-    }else if(array.length == 0 && total == 0) {
+function preOperate() {
+    if(array[0] == 0 || array.length == 0) {
         array.splice(0,0, visual.textContent);
-        operator = "+";
         visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator !== "+") {
+    }else if(array[0] !== 0 && operator !== null) {
         operate();
-        operator = "+";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator == "+") {
-        operate()
-        operator = "+"
-        visual.textContent = 0;
-    }
-}
-
-subtraction.onclick = () => {
-    hasDecimal = false;
-    if(total !== 0) {
-        array.splice(0,0, parseFloat(total));
-        operator = "-"
-        total = 0;
-        visual.textContent = 0;
-    }else if(array.length == 0 && total == 0) {
-        array.splice(0,0, visual.textContent);
-        operator = "-";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator !== "-") {
-        operate();
-        operator = "-";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator == "-") {
-        operate()
-        operator = "-"
-        visual.textContent = 0;
-    }
-}
-
-multiplication.onclick = () => {
-    hasDecimal = false;
-    if(total !== 0){
-        array.splice(0,0, parseFloat(total));
-        operator = "*"
-        total = 0;
-        visual.textContent = 0;
-    }else if(array.length == 0 && total == 0){
-        array.splice(0,0, visual.textContent);
-        operator = "*";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator !== "*"){
-        operate();
-        operator = "*";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator == "*"){
-        operate()
-        operator = "*"
-        visual.textContent = 0;
-    }
-}
-
-division.onclick = () => {
-    hasDecimal = false;
-    if(total !== 0){
-        array.splice(0,0, parseFloat(total));
-        operator = "/"
-        total = 0;
-        visual.textContent = 0;
-    }else if(array.length == 0 && total == 0){
-        array.splice(0,0, visual.textContent);
-        operator = "/";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator !== "/"){
-        operate();
-        operator = "/";
-        visual.textContent = 0;
-    }else if(array.length !== 0 && total == 0 && operator == "/"){
-        operate()
-        operator = "/"
-        visual.textContent = 0;
+        visual.textContent = array[0];
+        runningTotal = true;
     }
 }
 
 equals.onclick = () => {
+    if(array.length !== 0){
     operate();
-    total = array[0];
-    visual.textContent = total;
+    visual.textContent = array[0];
+    }
 }
 
 function operate(){
@@ -256,19 +155,24 @@ function operate(){
         array[0] = parseFloat(array[0]) / parseFloat(visual.textContent);
         }
     }
-   isPercent = false;
-   hasDecimal = false;
+    if(visual.textContent.includes(".") && array[0].length > 23){
+        array[0].toFixed(23)
+        visual.textContent = Math.round((parseFloat(array[0]) * 100) / 100);
+    }
+    operator = null;
+    isPercent = false;
+    hasDecimal = false;
+    runningTotal = true;
 }
 
-const activeClear = document.querySelector("#ac");
+const activeClear = document.querySelector(".ac");
 activeClear.onclick = () => {
     visual.textContent = 0;
 }
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", () =>{
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
     visual.textContent = 0;
-    total = 0;
     array.length = 0;
     hasDecimal = false;
     isPercent = false;
@@ -276,4 +180,3 @@ clear.addEventListener("click", () =>{
         button.disabled = false;
       });
 });
-
